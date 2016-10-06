1a6ddecfbaf38d643a7620a8fdc90694424a29f5
Jordan Kloosterman <Jordanklooster+Github@gmail.com>
2016-10-05 22:25:20 -0400

    Added Game Files
    Finally found out how github making repositories works, now I know it
    creates a new folder where I tell it to create a new repo. I thought it
    would just us the folder I selected.

---
+++ Assets/Character_Controls.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Controls : MonoBehaviour {
+
+    //This is Pragmatic Boots
+
+    Rigidbody2D rb;
+
+    public float MaxRunSpeed = 2;
+    public float MaxForce;
+    public float FMagnitude = 20;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+        // make AddForce work Using a function based on velocity
+
+        rb.AddForce( new Vector2( Input.GetAxisRaw("Horizontal") * (MaxRunSpeed - Mathf.Abs(rb.velocity.x) ) , 0) * Time.deltaTime * FMagnitude);
+        
+
+    }
+}
